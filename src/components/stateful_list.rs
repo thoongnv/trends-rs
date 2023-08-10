04e91bebc283a74fbc16c90768c4b10f76ef2770
Thong Nguyen <thoongnv@gmail.com>
2023-08-10 14:55:11 +0700

    Comment out unused compoment StatefulList

--- src/components/stateful_list.rs
+++ src/components/stateful_list.rs
@@ -5,87 +5,87 @@ use crate::{
 };
 
 use crossterm::event::{Event, KeyCode};
-use ratatui::widgets::ListState;
+// use ratatui::widgets::ListState;
 
 // https://github.com/ratatui-org/ratatui/blob/v0.22.0/examples/list.rs
-#[derive(Debug)]
-pub struct StatefulList<T> {
-    pub state: ListState,
-    pub items: Vec<T>,
-    pub focused: bool,
-}
+// #[derive(Debug)]
+// pub struct StatefulList<T> {
+//     pub state: ListState,
+//     pub items: Vec<T>,
+//     pub focused: bool,
+// }
 
-impl<T> StatefulList<T> {
-    pub fn new() -> StatefulList<T> {
-        StatefulList {
-            state: ListState::default(),
-            items: vec![],
-            focused: false,
-        }
-    }
+// impl<T> StatefulList<T> {
+//     pub fn new() -> StatefulList<T> {
+//         StatefulList {
+//             state: ListState::default(),
+//             items: vec![],
+//             focused: false,
+//         }
+//     }
 
-    pub fn set_items(&mut self, items: Vec<T>) {
-        self.items = items;
-    }
+//     pub fn set_items(&mut self, items: Vec<T>) {
+//         self.items = items;
+//     }
 
-    fn next(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i >= self.items.len() - 1 {
-                    0
-                } else {
-                    i + 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
-    }
+//     fn next(&mut self) {
+//         let i = match self.state.selected() {
+//             Some(i) => {
+//                 if i >= self.items.len() - 1 {
+//                     0
+//                 } else {
+//                     i + 1
+//                 }
+//             }
+//             None => 0,
+//         };
+//         self.state.select(Some(i));
+//     }
 
-    fn previous(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i == 0 {
-                    self.items.len() - 1
-                } else {
-                    i - 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
-    }
+//     fn previous(&mut self) {
+//         let i = match self.state.selected() {
+//             Some(i) => {
+//                 if i == 0 {
+//                     self.items.len() - 1
+//                 } else {
+//                     i - 1
+//                 }
+//             }
+//             None => 0,
+//         };
+//         self.state.select(Some(i));
+//     }
 
-    fn unselect(&mut self) {
-        self.state.select(None);
-    }
-}
+//     fn unselect(&mut self) {
+//         self.state.select(None);
+//     }
+// }
 
-impl<T> Component for StatefulList<T> {
-    fn handle_events(&mut self, event: Event, state: &mut AppState) {
-        match event {
-            Event::Key(key_event) => match key_event.code {
-                KeyCode::Left => self.unselect(),
-                KeyCode::Down => self.next(),
-                KeyCode::Up => self.previous(),
-                KeyCode::Esc => {
-                    state.unfocused = true;
-                    self.focused = false;
-                }
-                _ => {}
-            },
-            _ => {}
-        }
-    }
+// impl<T> Component for StatefulList<T> {
+//     fn handle_events(&mut self, event: Event, state: &mut AppState) {
+//         match event {
+//             Event::Key(key_event) => match key_event.code {
+//                 KeyCode::Left => self.unselect(),
+//                 KeyCode::Down => self.next(),
+//                 KeyCode::Up => self.previous(),
+//                 KeyCode::Esc => {
+//                     state.unfocused = true;
+//                     self.focused = false;
+//                 }
+//                 _ => {}
+//             },
+//             _ => {}
+//         }
+//     }
 
-    fn focused(&self) -> bool {
-        self.focused
-    }
+//     fn focused(&self) -> bool {
+//         self.focused
+//     }
 
-    fn set_focus(&mut self, focus: bool) {
-        self.focused = focus;
-    }
-}
+//     fn set_focus(&mut self, focus: bool) {
+//         self.focused = focus;
+//     }
+// }
 
 // Custom stateful list support multi select items
 #[derive(Debug)]
